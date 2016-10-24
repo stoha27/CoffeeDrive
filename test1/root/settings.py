ea168f8770557139cfe6fffede0722fe48379b96
vasyavin <vasyavin17@gmail.com>
2016-10-24 19:26:23 +0300

    qwqwert

--- test1/root/settings.py
+++ test1/root/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registr.apps.CoffeeShopConfig',
-    'clientorder'
+    'clientorder',
+     'start',
 )
 
 MIDDLEWARE_CLASSES = (
--- test1/root/urls.py
+++ test1/root/urls.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 urlpatterns = [
         url(r'^admin/', admin.site.urls),
         url(r'^registr/', include('registr.urls')),
+        url(r'^', include('start.urls')),
+        url(r'^i18n/', include('django.conf.urls.i18n')),
+        url(r'^rosetta/', include('rosetta.urls')),
 ]
