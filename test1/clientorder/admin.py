ec46a7701162b043366794e023377974fe09fe11
NewAleksandr <NewAleksandr@gmail.com>
2016-10-25 12:18:18 +0300

    adding some changes after installing rosetta from colaborator Kushnir

--- test1/clientorder/admin.py
+++ test1/clientorder/admin.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from clientorder.models import Client, Goods, Order, UserProfile, GroupOrder
 
 
-
-
 # Register your models here.
 class OrderAdmin(admin.ModelAdmin):
     field = ['count', 'name_Goods', 'date_of_order']
-    
+
+
 admin.site.register(Goods)
 admin.site.register(Client)
 
-admin.site.register(Order, OrderAdmin) 
+admin.site.register(Order, OrderAdmin)
 
 
  
